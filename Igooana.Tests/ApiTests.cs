d9f3d4d552e126438938f2ad1e14767a470b19ed
Valentin Vasilyev <valentin.vasilyev@outlook.com>
2013-12-01 12:07:09 +0400

    First unit test

---
+++ Igooana.Tests/ApiTests.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Igooana.Tests {
+  [TestClass]
+  public class ApiTests {
+    [TestMethod]
+    public void ItCreatesNewApiInstanceWithConstructor() {
+      var api = new Api("fake", "fake");
+      Assert.IsNotNull(api);
+    }
+  }
+}
